48f707eb471f3917faa3d8f7b0f71d325cf79476
AnsonCar <anson1tsang@gmail.com>
2024-09-01 12:49:37 -0300

    refactor: rename file

--- openapi_client_builder/tools/template/nuxt.py
+++ openapi_client_builder/tools/template/nuxt.py
@@ -17,15 +17,12 @@ export function getCookie(cname: string) {
 }
 
 export async function api(url: string, options: RequestInit | undefined) {
-  // 取得 url 資源 和 選項
   const BasisApiURL = "http://localhost:8000"
   try {
-      // 請求
       const res = await fetch(`${BasisApiURL}${url}`, options)
       const data = await res.json()
       return data
   } catch (error) {
-      // 如果 請求失敗（Server 死咗 ）
       return {
           "error": "An error occurred.",
           "code": 500,
@@ -35,15 +32,12 @@ export async function api(url: string, options: RequestInit | undefined) {
 }
 
 export async function apiFile(url: string, options: RequestInit | undefined) {
-  // 取得 url 資源 和 選項
   const BasisApiURL = "http://localhost:8000"
   try {
-      // 請求
       const res = await fetch(`${BasisApiURL}/api/${url}`, options)
-      const blob = await res.blob(); // 提取实际的文件内容
+      const blob = await res.blob();
       return blob;
   } catch (error) {
-      // 如果 請求失敗（Server 死咗 ）
       return {
           "error": "An error occurred.",
           "code": 500,
